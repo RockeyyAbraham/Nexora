bcd7a8f9e29c320a8254c555f6a48828e1967f22
manyaks005 <196074603+manyaks005@users.noreply.github.com>
2026-06-28 05:14:53 +0530

    added product model

---
+++ backend/models/Product.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);
